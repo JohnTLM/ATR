f7b8f34cfe5922fea6eb662e8d0d6a3fa26cac06
JohnTLM <60571852+JohnTLM@users.noreply.github.com>
2023-05-13 20:15:37 +0800

    Add files via upload

--- st_atr_pdf_langchain.py
+++ st_atr_pdf_langchain.py
@@ -12,8 +12,9 @@ from langchain.llms import OpenAI
 
 
 def main():
-    load_dotenv()  # take environment variables from .env.
-    OPEN_API_KEY = os.getenv("OPEN_API_KEY")
+    # load_dotenv()  # take environment variables from .env.
+    # API_KEY = os.getenv("API_KEY")
+
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask the 5 major must read ATR files:💯 ")
 
@@ -31,19 +32,30 @@ def main():
         chunk = text_splitter.split_text(text)
         st.write(chunk)
 
-        embeddings = OpenAIEmbeddings(openai_api_key=OPEN_API_KEY)
-        knowledge_base = faiss.FAISS.from_texts(chunk, embeddings)
+        API_KEY = st.text_input(
+            "Please key in your own OPEN API Key :sunglasses: :", type="password"
+        )
 
-        # show user input
-        user_question = st.text_input("Ask a question about your PDF")
-        if user_question:
-            docs = knowledge_base.similarity_search(user_question)
-            st.write(docs)
+        try:
+            if API_KEY:
+                embeddings = OpenAIEmbeddings(openai_api_key=API_KEY)
+                knowledge_base = faiss.FAISS.from_texts(chunk, embeddings)
 
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            response = chain.run(input_documents=docs, question=user_question)
-            st.write(response)
+                # show user input
+                user_question = st.text_input("Ask a question about your PDF")
+                if user_question:
+                    docs = knowledge_base.similarity_search(user_question)
+                    st.write(docs)
+
+                    llm = OpenAI(openai_api_key=API_KEY)
+                    chain = load_qa_chain(llm=llm, chain_type="stuff")
+                    response = chain.run(input_documents=docs, question=user_question)
+                    st.write(response)
+        except Exception as e:
+            error_msg = (
+                f"**Exception message: {str(e)}\n Exception type: {type(e).__name__ }**"
+            )
+            st.write(":warning:", error_msg, ":warning:")
 
 
 if __name__ == "__main__":
